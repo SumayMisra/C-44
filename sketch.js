52b3d2871c7cca9dedebb25c1c85fe2f520d5e66
SumayMisra <63454749+SumayMisra@users.noreply.github.com>
2020-06-25 13:53:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var bg,box,coin;
+
+function preload(){
+
+bg=loadImage("background.webp");
+//box=loadImage(box.webp);
+//coin=loadImage(coin.webp);
+
+
+}
+
+function setup(){
+createCanvas(1000,500);
+
+
+
+}
+
+function draw(){
+
+background(bg);
+
+}
+
+
